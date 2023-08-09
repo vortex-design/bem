238fe051d8b48ab25452effa466549551c9ed536
mickmao <141818035+mickmao@users.noreply.github.com>
2023-08-09 04:50:09 -0500

    Add grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar/bem.pest"]
+pub struct BEMParser;
+
 fn main() {
-    println!("Hello, world!");
+	let bem_content = "media-player(dark|light)\nbutton(fast-forward|rewind)\ntimeline";
+	let parsed = BEMParser::parse(Rule::bem, bem_content).expect("Failed to parse BEM content");
+
+	// Process the parsed content...
+	for pair in parsed {
+		println!("{:?}", pair);
+	}
 }
